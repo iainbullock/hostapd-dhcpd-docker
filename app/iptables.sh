53d644c0d294552ef7667e933a5ff48fcc252d1d
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-08-05 18:37:41 +0100

    Update iptables.sh

--- app/iptables.sh
+++ app/iptables.sh
@@ -2,4 +2,4 @@
 iptables-nft -t nat -C POSTROUTING -o $UPLINK_IFACE -j MASQUERADE || iptables-nft -t nat -A POSTROUTING -o $UPLINK_IFACE -j MASQUERADE
 iptables-nft -C FORWARD -i $UPLINK_IFACE -o $AP_IFACE -m state --state RELATED,ESTABLISHED -j ACCEPT || iptables-nft -A FORWARD -i $UPLINK_IFACE -o $AP_IFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables-nft -C FORWARD -i $AP_IFACE -o $UPLINK_IFACE -j ACCEPT || iptables-nft -A FORWARD -i $AP_IFACE -o $UPLINK_IFACE -j ACCEPT
-iptables -C FORWARD -i $UPLINK_IFACE -p icmp -j ACCEPT || iptables-nft -iptables -A FORWARD -i $UPLINK_IFACE -p icmp -j ACCEPT
+iptables-nft -C FORWARD -i $UPLINK_IFACE -p icmp -j ACCEPT || iptables-nft -iptables -A FORWARD -i $UPLINK_IFACE -p icmp -j ACCEPT
