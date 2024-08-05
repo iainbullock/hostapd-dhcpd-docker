f29b0fb05f188303d5d76b2ad4aa520288081c05
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-08-05 10:40:31 +0100

    Update iptables_off.sh

--- app/iptables_off.sh
+++ app/iptables_off.sh
@@ -1,4 +1,4 @@
 #/bin/sh
-iptables-nft -t nat -C POSTROUTING -o eth0 -j MASQUERADE && iptables-nft -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-iptables-nft -C FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT && iptables-nft -D FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables-nft -C FORWARD -i wlan0 -o eth0 -j ACCEPT && iptables-nft -D FORWARD -i wlan0 -o eth0 -j ACCEPT
+iptables-nft -t nat -C POSTROUTING -o $UPLINK_IFACE -j MASQUERADE && iptables-nft -t nat -D POSTROUTING -o $UPLINK_IFACE -j MASQUERADE
+iptables-nft -C FORWARD -i $UPLINK_IFACE -o $AP_IFACE -m state --state RELATED,ESTABLISHED -j ACCEPT && iptables-nft -D FORWARD -i $UPLINK_IFACE -o $AP_IFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables-nft -C FORWARD -i $AP_IFACE -o $UPLINK_IFACE -j ACCEPT && iptables-nft -D FORWARD -i $AP_IFACE -o $UPLINK_IFACE -j ACCEPT
